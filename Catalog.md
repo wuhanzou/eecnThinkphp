cc4afcbcbb356756cfa4041aac894a55300022e6
zou <195474368@qq.com>
2016-10-17 17:21:13 +0800

    装备工作

---
+++ Catalog.md
@@ -0,0 +1,106 @@
+﻿网站目录结构
+
+www  WEB部署目录（或者子目录）
+│
+├─index.php       前台入口文件
+│
+├─admin.php       后台入口文件
+│
+├─favicon.ico     ico小图标
+│
+├─readme.html     网站说明 
+│
+├─README.md       README备注文件
+│
+├─.htaccess       路由器配置文件(apache专用)(可省略)
+│
+├─Addons 插件目录
+│  
+├─Application 应用模块目录
+│  │
+│  ├─Admin 后台模块
+│  │  │
+│  │  ├─Conf 后台配置文件目录
+│  │  ├─Common 后台函数公共目录
+│  │  ├─Controller 后台控制器目录
+│  │  ├─Model 后台模型目录
+│  │  ├─Logic 后台模型逻辑目录
+│  │  └─View 后台视图文件目录
+│  │  
+│  ├─Common 公共模块目录（不能直接访问）
+│  │  │
+│  │  ├─Conf 公共配置文件目录
+│  │  ├─Common 公共函数文件目录
+│  │  ├─Controller 模块访问控制器目录
+│  │  └─Model 公共模型目录
+│  │  
+│  ├─Home Home 前台模块
+│  │  │
+│  │  ├─Conf 前台配置文件目录
+│  │  ├─Common 前台函数公共目录
+│  │  ├─Controller 前台控制器目录
+│  │  ├─Model 前台模型目录
+│  │  ├─View 模块视图文件目录
+│  │  ├─Logic 后台模型逻辑目录
+│  │  └─Widget 小工具(广告位,表单)
+│  │
+│  └─User 用户模块（不能直接访问）
+│     │
+│     ├─Api  用户登录及第三方登录接口文件目录(QQ帐号,新浪帐号)
+│     ├─Conf 用户配置目录
+│     ├─Common 后台函数公共目录
+│     ├─Model 用户模型目录
+│     └─Service 用户Service文件目录
+│
+├─Public 应用资源文件目录
+│  │
+│  ├─Admin 后台资源文件目录
+│  │  │ 
+│  │  ├─css 样式文件目录
+│  │  ├─images 图片文件目录
+│  │  └─js 脚本文件目录
+│  │
+│  ├─Home 前台资源文件目录
+│  │  │ 
+│  │  ├─css 样式文件目录
+│  │  ├─images 图片文件目录
+│  │  └─js 脚本文件目录
+│  │
+│  └─static 公共资源文件目录及第三方插件(jq) 
+│ 
+├─Runtime 应用运行时目录
+│
+├─Uploads 上传根目录
+│  │
+│  ├─Download 文件上传目录
+│  ├─Picture 图片上传目录
+│  ├─Editor 编辑器图片上传目录
+│  └─Attachment  附件插件上传目录
+│
+└─ThinkPHP        框架系统目录（可以部署在非web目录下面）
+  │	
+  ├─Common       核心公共函数目录
+  │
+  ├─Conf         核心配置目录 
+  │
+  ├─Lang         核心语言包目录
+  │
+  ├─Library      框架类库目录
+  │  │
+  │  ├─Think     核心Think类库包目录
+  │  ├─Behavior  行为类库目录
+  │  ├─Org       Org类库包目录
+  │  ├─Vendor    第三方类库目录
+  │  ├─ ...      更多类库目录
+  │  │
+  ├─Mode         框架应用模式目录
+  │
+  ├─Tpl          系统模板目录
+  │
+  ├─LICENSE.txt  框架授权协议文件
+  │
+  ├─logo.png     框架LOGO文件
+  │
+  ├─README.txt   框架README文件
+  │
+  └─ThinkPHP.php    框架入口文件
---
+++ install.sql
@@ -0,0 +1,39 @@
+-- -----------------------------
+-- Think MySQL Data Transfer
+-- Date : 2013-12-17 15:14:46
+-- -----------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+-- -----------------------------
+-- Table structure for `onethink_action`
+-- -----------------------------
+DROP TABLE IF EXISTS `onethink_action`;
+CREATE TABLE `onethink_action` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` char(30) NOT NULL DEFAULT '' COMMENT '行为唯一标识',
+  `title` char(80) NOT NULL DEFAULT '' COMMENT '行为说明',
+  `remark` char(140) NOT NULL DEFAULT '' COMMENT '行为描述',
+  `rule` text NULL  COMMENT '行为规则',
+  `log` text NULL  COMMENT '日志规则',
+  `type` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '类型',
+  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态',
+  `update_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='系统行为表';
+
+-- -----------------------------
+-- Records of `onethink_action`
+-- -----------------------------
+INSERT INTO `onethink_action` VALUES ('1', 'user_login', '用户登录', '积分+10，每天一次', 'table:member|field:score|condition:uid={$self} AND status>-1|rule:score+10|cycle:24|max:1;', '[user|get_nickname]在[time|time_format]登录了后台', '1', '1', '1387181220');
+INSERT INTO `onethink_action` VALUES ('2', 'add_article', '发布文章', '积分+5，每天上限5次', 'table:member|field:score|condition:uid={$self}|rule:score+5|cycle:24|max:5', '', '2', '0', '1380173180');
+INSERT INTO `onethink_action` VALUES ('3', 'review', '评论', '评论积分+1，无限制', 'table:member|field:score|condition:uid={$self}|rule:score+1', '', '2', '1', '1383285646');
+INSERT INTO `onethink_action` VALUES ('4', 'add_document', '发表文档', '积分+10，每天上限5次', 'table:member|field:score|condition:uid={$self}|rule:score+10|cycle:24|max:5', '[user|get_nickname]在[time|time_format]发表了一篇文章。\r\n表[model]，记录编号[record]。', '2', '0', '1386139726');
+INSERT INTO `onethink_action` VALUES ('5', 'add_document_topic', '发表讨论', '积分+5，每天上限10次', 'table:member|field:score|condition:uid={$self}|rule:score+5|cycle:24|max:10', '', '2', '0', '1383285551');
+INSERT INTO `onethink_action` VALUES ('6', 'update_config', '更新配置', '新增或修改或删除配置', '', '', '1', '1', '1383294988');
+INSERT INTO `onethink_action` VALUES ('7', 'update_model', '更新模型', '新增或修改模型', '', '', '1', '1', '1383295057');
+INSERT INTO `onethink_action` VALUES ('8', 'update_attribute', '更新属性', '新增或更新或删除属性', '', '', '1', '1', '1383295963');
+INSERT INTO `onethink_action` VALUES ('9', 'update_channel', '更新导航', '新增或修改或删除导航', '', '', '1', '1', '1383296301');
+INSERT INTO `onethink_action` VALUES ('10', 'update_menu', '更新菜单', '新增或修改或删除菜单', '', '', '1', '1', '1383296392');
+INSERT INTO `onethink_action` VALUES ('11', 'update_category', '更新分类', '新增或修改或删除分类', '', '', '1', '1', '1383296765');
