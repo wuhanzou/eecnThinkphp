456f8be6fbf98465a8928338452fa4845fe70228
Home_zou <xzhb_1985@126.com>
2016-11-08 00:35:43 +0800

    首页控制器整理

--- Application/Admin/Controller/ConfigController.class.php
+++ Application/Admin/Controller/ConfigController.class.php
@@ -1,12 +1,32 @@
 <?php
 namespace Admin\Controller;
-use Think\Controller;
-class ConfigController extends Controller {
+/**
+ * 后台配置控制器
+ * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+ */
+class ConfigController extends AdminController {
     /**
-     * 用户详情
+     * 配置管理
      * @author 麦当苗儿 <zuojiazi@vip.qq.com>
      */
     public function index(){
+        /* 查询条件初始化 */
+        $map = array();
+        $map  = array('status' => 1);
+        if(isset($_GET['group'])){
+            $map['group']   =   I('group',0);
+        }
+        if(isset($_GET['name'])){
+            $map['name']    =   array('like', '%'.(string)I('name').'%');
+        }
+
+        $list = $this->lists('Config', $map,'sort,id');
+        // 记录当前列表页的cookie,$_SERVER["REQUEST_URI"]获取当前的完整url
+        Cookie('__forward__',$_SERVER['REQUEST_URI']);
+        $this->assign('group',C('CONFIG_GROUP_LIST'));
+        $this->assign('group_id',I('get.group',0));
+        $this->assign('list', $list);
+        $this->meta_title = '配置管理';
         $this->display();
     }
 
